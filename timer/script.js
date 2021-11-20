e1a5c306106a1de6ccd01e12b177ef488e64cd9e
SuS <73340172+IPOOO9@users.noreply.github.com>
2021-11-20 13:36:01 +1000

    Update script.js

--- timer/script.js
+++ timer/script.js
@@ -2,45 +2,42 @@ let start = document.querySelector('.start');
 let countdown = document.querySelector('.score');
 let minute, seconds, milliseconds;
 
-let audio = new Audio('metal.mp3');
 
-//Регистрация нажатия запуска таймера
-start.addEventListener('click', count);
 
-//Обратный отсчет
-function count() {
+start.addEventListener('click', TimerStart);
+
+
+function TimerStart() {
     start.innerText = 'ПЕРЕЗАПУСТИТЬ';
     document.getElementById("img").hidden=true;
     document.getElementById("end").hidden=true;
 
     minute = 1;
-    seconds = 3;
+    seconds = 0;
     milliseconds = 0;
 
-    //Вызов Promise (resolve - успешно, reject - ошибка)
+   
     const promise = new Promise((resolve, reject) => {
         const interval = setInterval(() => {
-            audio.play();
-            updateCountdown();
+            updateTimer();
 
-            //обработка миллисекунд
             milliseconds -= 84;
             if (milliseconds <= 0) {
                 milliseconds = 999;
                 seconds--;
             }
-            //обработка секунд
+
             if (seconds <= 0) {
                 milliseconds = 999;
                 seconds = 59;
                 minute--;
             }
-            //обработка минут
+
             if (minute < 0) {
                 minute = 0;
                 seconds = 0;
                 milliseconds = 0;
-                updateCountdown();
+                updateTimer();
                 clearInterval(interval);
                 resolve('Ok');
             }
@@ -57,7 +54,7 @@ function count() {
     });
 }
 
-//Обновление таймера на экране
-function updateCountdown() {
+
+function updateTimer() {
     countdown.innerText = `${('00' + minute).slice(-2)}:${('00' + seconds).slice(-2)}:${('000' + milliseconds).slice(-3)}`;
 }
