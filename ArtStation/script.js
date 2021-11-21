6082ae22671b9cda90bc15fb517bad0cb96ab81e
SuS <73340172+IPOOO9@users.noreply.github.com>
2021-11-21 15:41:51 +1000

    Update script.js

--- ArtStation/script.js
+++ ArtStation/script.js
@@ -2,7 +2,7 @@ function SpawnImage() {
     document.getElementById("button").hidden=true;
     fetch('pic.json').then(response => {
         response.json().then(result => {
-            let arr = [result.S89, result.SPower,result.Assault,result.FuckNato];
+            let arr = [result.S89, result.SPower,result.Assault,result.FuckNATO];
             for (let i = 0; i < 4; i++) {
                     let pic = new Image();
                     pic.src = arr[i];
