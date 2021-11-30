9253a8c5a882ba71535fae3b32cc9ae1300bed65
SuS <73340172+IPOOO9@users.noreply.github.com>
2021-11-30 19:18:39 +1000

    Add files via upload

---
+++ npm/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+const log = console.log;
+
+log(chalk.rgb(255,255,255).bgWhite.bold('КИЕВ'));
+log(chalk.rgb(255,255,255).bgBlue.bold('РУССКИЙ'));
+log(chalk.rgb(255,255,255).bgRed.bold('ГОРОД'));
