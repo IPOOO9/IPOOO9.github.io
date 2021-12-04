f749ddae18156fc350535c49057112c27c0f4a0c
SuS <73340172+IPOOO9@users.noreply.github.com>
2021-12-04 22:35:22 +1000

    Add files via upload

---
+++ WebPack/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+import "./dist/style.css";
+const log = console.log;
+
+log(chalk.rgb(255,255,255).bgWhite.bold('ЭТО'));
+log(chalk.rgb(255,255,255).bgBlue.bold('ПРОСТО'));
+log(chalk.rgb(255,255,255).bgRed.bold('ТЕСТ'));
