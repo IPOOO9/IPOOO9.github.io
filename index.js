e1738bd20c54829a8e6f7dd97ecc41768fbdce5c
SuS <73340172+IPOOO9@users.noreply.github.com>
2021-12-04 22:32:08 +1000

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import chalk from 'chalk';
-import "./dist/style.css";
-const log = console.log;
-
-log(chalk.rgb(255,255,255).bgWhite.bold('ЭТО'));
-log(chalk.rgb(255,255,255).bgBlue.bold('ПРОСТО'));
-log(chalk.rgb(255,255,255).bgRed.bold('ТЕСТ'));
